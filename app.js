26c830e6d5c73162785accda9d421500dcabdae8
zioan <zaharia.ioan@gmail.com>
2023-02-18 14:49:01 +0100

    cleanup

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
-const express = require('express');
-require('dotenv/config');
-require('./db');
-const app = express();
-const PORT = process.env.PORT;
-const cookieParser = require('cookie-parser');
-app.use(cookieParser());
+const express = require('express')
+require('dotenv/config')
+require('./db')
+const app = express()
+const PORT = process.env.PORT
+const cookieParser = require('cookie-parser')
+app.use(cookieParser())
 
-const morgan = require('morgan');
-const cors = require('cors');
+const morgan = require('morgan')
+const cors = require('cors')
 
-app.use(express.json());
-app.use(morgan('tiny'));
+app.use(express.json())
+app.use(morgan('tiny'))
 app.use(
   cors({
     origin: [
@@ -18,19 +18,19 @@ app.use(
       'https://snippet-manager-test.netlify.app',
     ],
     credentials: true,
-  })
-);
+  }),
+)
 
-const usersRouter = require('./routers/users');
-const tagsRouter = require('./routers/tags');
-const snippetsRouter = require('./routers/snippets');
+const usersRouter = require('./routers/users')
+const tagsRouter = require('./routers/tags')
+const snippetsRouter = require('./routers/snippets')
 
-app.use(`/users`, usersRouter);
-app.use(`/tags`, tagsRouter);
-app.use(`/snippets`, snippetsRouter);
+app.use(`/users`, usersRouter)
+app.use(`/tags`, tagsRouter)
+app.use(`/snippets`, snippetsRouter)
 
 // for development
-app.listen(PORT, () => console.log(`Server running on ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on ${PORT}`))
 
 // for production
 // app.listen(() => console.log(`Server running`));
