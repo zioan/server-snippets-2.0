d29ed8780cf80c2e2817f19550c86e8464d03355
zioan <zaharia.ioan@gmail.com>
2023-02-19 13:38:18 +0100

    cors

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ app.use(
     origin: [
       'http://localhost:3000',
       'https://snippet-manager-test.netlify.app',
+      'https://snippets.zioan.com',
     ],
     credentials: true,
   }),
