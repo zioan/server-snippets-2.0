a49871b5f5d2c516b2a2594659e3f16a1e53aa47
zioan <zaharia.ioan@gmail.com>
2024-11-05 12:44:51 +0100

    Add GitHub Actions workflow for automated deployment

--- app.js
+++ app.js
@@ -1,37 +1,33 @@
-const express = require('express')
-require('dotenv/config')
-require('./db')
-const app = express()
-const PORT = process.env.PORT
-const cookieParser = require('cookie-parser')
-app.use(cookieParser())
+const express = require("express");
+require("dotenv/config");
+require("./db");
+const app = express();
+const PORT = process.env.PORT;
+const cookieParser = require("cookie-parser");
+app.use(cookieParser());
 
-const morgan = require('morgan')
-const cors = require('cors')
+const morgan = require("morgan");
+const cors = require("cors");
 
-app.use(express.json())
-app.use(morgan('tiny'))
+app.use(express.json());
+app.use(morgan("tiny"));
 app.use(
   cors({
-    origin: [
-      'http://localhost:3000',
-      'https://snippet-manager-test.netlify.app',
-      'https://snippets.zioan.com',
-    ],
+    origin: ["http://localhost:3000", "http://127.0.0.1:3000/", "https://snippet-manager-test.netlify.app", "https://snippets.zioan.com"],
     credentials: true,
-  }),
-)
+  })
+);
 
-const usersRouter = require('./routers/users')
-const tagsRouter = require('./routers/tags')
-const snippetsRouter = require('./routers/snippets')
+const usersRouter = require("./routers/users");
+const tagsRouter = require("./routers/tags");
+const snippetsRouter = require("./routers/snippets");
 
-app.use(`/users`, usersRouter)
-app.use(`/tags`, tagsRouter)
-app.use(`/snippets`, snippetsRouter)
+app.use(`/users`, usersRouter);
+app.use(`/tags`, tagsRouter);
+app.use(`/snippets`, snippetsRouter);
 
 // for development
-app.listen(PORT, () => console.log(`Server running on ${PORT}`))
+app.listen(PORT, () => console.log(`Server running on ${PORT}`));
 
 // for production
 // app.listen(() => console.log(`Server running`));
